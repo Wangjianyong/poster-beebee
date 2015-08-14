d94f0f98e03f5693007d3b0db706592eaa5f5acf
Jhon <jianywan@gmail.com>
2015-08-14 10:01:54 +0800

    Update test.py

--- test.py
+++ test.py
@@ -5,6 +5,6 @@ import time
 
 if __name__ == '__main__':
     connector = zhihu.zhihu()
-    connector.login(account="wjy5095844@sina.com", password="wjy13852873220")
+    connector.login(account="wjy5095844@sina.com", password="**********")
     time.sleep(2)
     connector.logout()
